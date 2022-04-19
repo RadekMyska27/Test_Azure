ac1cf1594093474c20f86cb7c22e240ccbb7e1a7
Radek Myška <radek.myska@idealine.cz>
2022-04-19 17:08:36 +0200

    initial

---
+++ Tests_Azure/BlobStorageExample.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+
+namespace Tests_Azure
+{
+    public class BlobStorageExample
+    {
+        const string SampleFilePath = @"E:\Idealine\Quadient\test_PDF\10.1mb.pdf";
+
+        const string ConnectionString =
+            @"DefaultEndpointsProtocol=https;AccountName=testidealine;AccountKey=ZaHYr5EFw4QFT1LukCqkutKy33mWsEFqoecdU+SnZyvX7KZYeMx8xOmDi1uAfTGYySKXcyYofAq1VQlZsjZYLw==;EndpointSuffix=core.windows.net";
+
+        const string ContainerName = "test1";
+        const string BlobName = "testing_1.bin";
+
+        public void Init()
+        {
+            // create container at Azure
+            var container = new BlobContainerClient(ConnectionString, ContainerName);
+            container.DeleteBlobIfExists(BlobName);
+        }
+
+        public async Task UploadWithReadingAsync(BlobContainerClient container)
+        {
+            var sampleFile = new FileInfo(SampleFilePath);
+            Console.WriteLine($"Upload - local file {sampleFile.Name} size for upload: {sampleFile.Length}");
+
+            try
+            {
+                await using var localFileStream = File.OpenRead(SampleFilePath);
+                var blockBlobClient = BlockBlobClient(container);
+
+                await using var stream = await blockBlobClient.OpenWriteAsync(true);
+                await localFileStream.CopyToAsync(stream);
+
+                await GetBlobsByHierarchyAsPagesAsync(container);
+
+                await localFileStream.FlushAsync();
+                await stream.FlushAsync();
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("Reading after upload first thread");
+                Console.WriteLine("------------------");
+
+                await GetBlobsByHierarchyAsPagesAsync(container);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Upload exception - {ex}");
+            }
+        }
+
+        public async Task UploadAsync(BlobContainerClient container)
+        {
+            var sampleFile = new FileInfo(SampleFilePath);
+            Console.WriteLine($"Upload - local file {sampleFile.Name} size for upload: {sampleFile.Length}");
+
+            try
+            {
+                await using var localFileStream = File.OpenRead(SampleFilePath);
+                var blockBlobClient = BlockBlobClient(container);
+
+                await using var stream = await blockBlobClient.OpenWriteAsync(true);
+                await localFileStream.CopyToAsync(stream);
+
+                await localFileStream.FlushAsync();
+                await stream.FlushAsync();
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("Reading after upload");
+                Console.WriteLine("------------------");
+
+                await GetBlobsByHierarchyAsPagesAsync(container);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Upload exception - {ex}");
+            }
+        }
+
+        public async Task UploadAfterFullUploadAsync(BlobContainerClient container)
+        {
+            await UploadAsync(container);
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+
+        public async Task<IAsyncEnumerable<Page<BlobHierarchyItem>>> GetBlobsByHierarchyAsPagesAsync(
+            BlobContainerClient blobContainerClient)
+        {
+            var resultSegment = blobContainerClient.GetBlobsByHierarchyAsync(BlobTraits.None, BlobStates.Uncommitted)
+                .AsPages(default, 50);
+
+            await foreach (Azure.Page<BlobHierarchyItem> blobPage in resultSegment)
+            {
+                foreach (BlobHierarchyItem blobHierarchyItem in blobPage.Values)
+                {
+                    Console.WriteLine("Blob.Name " + blobHierarchyItem.Blob.Name);
+                    Console.WriteLine(
+                        "Blob.Properties.ContentLength " + blobHierarchyItem.Blob.Properties.ContentLength);
+                    Console.WriteLine("Blob.Properties.ETag " + blobHierarchyItem.Blob.Properties.ETag);
+                    Console.WriteLine("--------------");
+                }
+            }
+
+            return resultSegment;
+        }
+
+        public async Task ContinueWhenFullUpload(BlobContainerClient container)
+        {
+            do
+            {
+                Console.WriteLine($"Waiting for fully uploaded.");
+                Task.Delay(1000).Wait();
+            } while (!await IsFullUploadedByContentLength(container));
+        }
+
+        public async Task<bool> IsFullUploadedByContentLength(BlobContainerClient blobContainerClient)
+        {
+            var resultSegment = await GetBlobsByHierarchyAsPagesAsync(blobContainerClient);
+
+            await foreach (Azure.Page<BlobHierarchyItem> blobPage in resultSegment)
+            {
+                if (blobPage.Values.Any(blobHierarchyItem => blobHierarchyItem.Blob.Properties.ContentLength > 0))
+                {
+                    Console.WriteLine($"Blob is fully uploaded.");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public async Task GetBlobsByHierarchyAsPagesWithDelayAsync(BlobContainerClient blobContainerClient, int delay)
+        {
+            Task.Delay(delay).Wait();
+            await GetBlobsByHierarchyAsPagesAsync(blobContainerClient);
+        }
+
+        public BlockBlobClient BlockBlobClient(BlobContainerClient container)
+        {
+            var blockBlobClient = container.GetBlockBlobClient(BlobName);
+            return blockBlobClient;
+        }
+
+
+        public BlobContainerClient GetBlobContainerClient()
+        {
+            var container = new BlobContainerClient(ConnectionString, ContainerName);
+            return container;
+        }
+
+
+        // public async Task ReadAsync()
+        // {
+        //     //Task.Delay(5000).Wait();
+        //
+        //     var container = new BlobContainerClient(ConnectionString, ContainerName);
+        //
+        //     try
+        //     {
+        //         // Get a reference to a blob
+        //         var blob = container.GetBlobClient(BlobName);
+        //
+        //         var bufSize = 2 * 1024 * 1024;
+        //         Console.WriteLine("Read - before open for read");
+        //
+        //         var openedStream = await blob.OpenReadAsync(new BlobOpenReadOptions(false) { BufferSize = bufSize });
+        //         Console.WriteLine("Read - after open for read");
+        //
+        //         var fileStream = File.OpenWrite(SampleFilePathFromBlob);
+        //         openedStream.CopyTo(fileStream);
+        //         openedStream.Flush();
+        //         Console.WriteLine("Read - Read finished");
+        //
+        //         var length = new FileInfo(SampleFilePathFromBlob).Length;
+        //         Console.WriteLine($"Read - read file size: {length}");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine($"Read exception - {ex}");
+        //     }
+        //     finally
+        //     {
+        //         // Clean up after the test when we're finished
+        //         //await container.DeleteAsync();
+        //     }
+        // }
+
+        // public async Task ReadWithWaitForFullyUploadedAsync(int maxSeconds)
+        // {
+        //     string connectionString = ConnectionString;
+        //     BlobContainerClient container = new BlobContainerClient(connectionString, ContainerName);
+        //
+        //     try
+        //     {
+        //         // Get a reference to a blob
+        //         BlobClient blob = container.GetBlobClient(BlobName);
+        //         bool isFullUploaded = false;
+        //         do
+        //         {
+        //             maxSeconds--;
+        //             isFullUploaded = IsFullUploaded(blob);
+        //             if (maxSeconds <= 0)
+        //             {
+        //                 throw new Exception("Waiting for uploaded blob failed");
+        //             }
+        //
+        //             Console.WriteLine($"Waiting for fully uploaded {maxSeconds} s.");
+        //             Task.Delay(1000).Wait();
+        //         } while (!isFullUploaded);
+        //
+        //         int bufSize = 2 * 1024 * 1024;
+        //         Console.WriteLine("ReadWithWaitForFullyUploadedAsync - before open for read");
+        //         var openedStream = await blob.OpenReadAsync(new BlobOpenReadOptions(false) { BufferSize = bufSize });
+        //         Console.WriteLine("ReadWithWaitForFullyUploadedAsync - after open for read");
+        //         FileStream fileStream = File.OpenWrite(SampleFilePathFromBlob2);
+        //         openedStream.CopyTo(fileStream);
+        //         openedStream.Flush();
+        //         openedStream.Close();
+        //         Console.WriteLine("ReadWithWaitForFullyUploadedAsync - Read finished");
+        //
+        //         long length = new FileInfo(SampleFilePathFromBlob2).Length;
+        //         Console.WriteLine($"ReadWithWaitForFullyUploadedAsync - read file size: {length}");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine($"ReadWithWaitForFullyUploadedAsync exception - {ex}");
+        //     }
+        //     finally
+        //     {
+        //         // Clean up after the test when we're finished
+        //         //await container.DeleteAsync();
+        //     }
+        // }
+
+        // public async Task PossibleWriteErrorWithNoFlush()
+        // {
+        //     string connectionString = ConnectionString;
+        //     BlobContainerClient container = new BlobContainerClient(connectionString, ContainerName);
+        //
+        //     try
+        //     {
+        //         // Get a reference to a blob
+        //         BlockBlobClient blockBlobClient = container.GetBlockBlobClient(BlobName);
+        //         await using var blobStream = await blockBlobClient.OpenWriteAsync(true);
+        //         await using var localFileStream = File.OpenRead(SampleFilePath);
+        //         Console.WriteLine("PossibleWriteErrorWithNoFlush - after OpenWriteAsync");
+        //         await localFileStream.CopyToAsync(blobStream);
+        //         Console.WriteLine("PossibleWriteErrorWithNoFlush - after CopyToAsync");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //     }
+        // }
+    }
+}
---
+++ Tests_Azure/Program.cs
@@ -0,0 +1,129 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tests_Azure;
+
+var blobStorageExample = new BlobStorageExample();
+blobStorageExample.Init();
+
+// scenario 1 - one thread only upload
+OneThreadReadDuringAndAfterUploadScenario();
+
+// scenario 2 - two thread upload and reading after Delay
+// TwoThreadUploadReadingWithDelayDuringUploadScenario();
+
+// scenario 3 - simulate exception - condition not met
+// SimulateExceptionConditionNotMetScenario();
+
+// scenario 4 - waiting for full upload by checking content length of the uploaded blob
+// WaitUntilBlobHasSetContentLength();
+
+void OneThreadReadDuringAndAfterUploadScenario()
+{
+    var container = blobStorageExample.GetBlobContainerClient();
+    var tasks = new List<Task> { };
+
+    var upload1 = blobStorageExample.UploadWithReadingAsync(container);
+    tasks.Add(upload1);
+
+    ResolveScenario(tasks);
+}
+
+void TwoThreadUploadReadingWithDelayDuringUploadScenario()
+{
+    var container = blobStorageExample.GetBlobContainerClient();
+    var tasks = new List<Task> { };
+    const int delay = 1000;
+
+    var upload1 = blobStorageExample.UploadAsync(container);
+    tasks.Add(upload1);
+
+    Console.WriteLine("Reading from second thread");
+    var upload2 = blobStorageExample.GetBlobsByHierarchyAsPagesWithDelayAsync(container, delay);
+    tasks.Add(upload2);
+
+    ResolveScenario(tasks);
+}
+
+
+void SimulateExceptionConditionNotMetScenario()
+{
+    var container = blobStorageExample.GetBlobContainerClient();
+    var tasks = new List<Task> { };
+
+    var upload1 = blobStorageExample.UploadAsync(container);
+    tasks.Add(upload1);
+
+    Task.Delay(100).Wait();
+    var upload2 = blobStorageExample.UploadAsync(container);
+    tasks.Add(upload2);
+
+    ResolveScenario(tasks);
+}
+
+void WaitUntilBlobHasSetContentLength()
+{
+    var container = blobStorageExample.GetBlobContainerClient();
+    var tasks = new List<Task> { };
+    
+    var upload1 = blobStorageExample.UploadAsync(container);
+    tasks.Add(upload1);
+    
+    var readFullyUploadTask = blobStorageExample.ContinueWhenFullUpload(container).ContinueWith(async _ =>
+    {
+        await blobStorageExample.UploadAfterFullUploadAsync(container);
+    });
+    tasks.Add(readFullyUploadTask);
+
+    Task.WaitAll(tasks.ToArray());
+    Console.ReadLine();
+}
+
+void ResolveScenario(List<Task> tasks)
+{
+    Task.WaitAll(tasks.ToArray());
+
+    Console.WriteLine("Press Enter to exit");
+    Console.ReadLine();
+}
+
+// check if blob is fuly uploaded 
+// var readFullyUploadTask = blobStorageExample.ContinueWhenFullUpload(container).ContinueWith(_=>
+// {
+//     blobStorageExample.UploadSkymambaAsync(container);
+// });
+// tasks.Add(readFullyUploadTask);
+
+
+// await blobStorageExample.GetBlobHierarchyItemAsync(container, BlobStates.Uncommitted);
+// await blobStorageExample.GetBlobHierarchyItemAsync(container, BlobStates.None);
+
+
+// await blobStorageExample.GetBlobHierarchyItemAsync(container, BlobStates.DeletedWithVersions);
+// await blobStorageExample.GetBlobHierarchyItemAsync(container, BlobStates.Version);
+// await blobStorageExample.GetBlobHierarchyItemAsync(container, BlobStates.Deleted);
+
+// var readTask = blobStorageExample.ReadAsync();
+// tasks.Add(readTask);
+
+// var readFullyUploadTask = blobStorageExample.ReadWithWaitForFullyUploadedAsync(30).ContinueWith(_=>
+// {
+//     blobStorageExample.UploadSkymambaAsync();
+// });
+// tasks.Add(readFullyUploadTask);
+
+
+// scenario 2 - try simulate error with switch read and write - same as in OMS500 code
+
+// scenario 3
+//await blobStorageExample.UploadSkymambaAsync();
+//blobStorageExample.MatchPattern = "2022/2/8";
+//await blobStorageExample.ListBlobPerDate(DateTime.MinValue, DateTime.MaxValue);
+
+//var upload1 = blobStorageExample.PossibleWriteErrorWithNoFlush();
+//upload1.ContinueWith(t => { var r = blobStorageExample.ReadAsync(); readTask.Add(r); });
+//tasks.Add(upload1);
+//Task.WaitAll(tasks.ToArray());
+//Task.WaitAll(readTask.ToArray());
